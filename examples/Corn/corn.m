6fdd0fcadc54f16684351868eab4e69d1db76c41
Kevin Dunn <kevin.g.dunn@gsk.com>
2011-10-19 15:51:24 -0400

    Added coefficient plot

--- examples/Corn/corn.m
+++ examples/Corn/corn.m
@@ -17,5 +17,21 @@ Y.add_labels(2, {'Moisture', 'Oil', 'Protein', 'Starch'})
 plot(Y)
 
 pcaY = lvm({'Quality', Y},2);
-pls = lvm({'X', X, 'Y', Y}, 5);
+A = 5;
+pls = lvm({'X', X, 'Y', Y}, A);
 plot(pls)
+
+
+R = pls.W{1}*inv(pls.P{1}'*pls.W{1}); 
+C = pls.super.C;
+beta = R*C';
+
+figure
+plot(data.m5spec.axisscale{2}, beta(:,1:4), 'linewidth', 2)
+title(['Coefficients using A=', num2str(A)])
+hold on
+w = data.m5spec.axisscale{2};
+plot([w(1) w(end)], [0, 0], 'k', 'linewidth', 3) 
+legend({'Moisture', 'Oil', 'Protein', 'Starch'})
+axis tight
+grid
