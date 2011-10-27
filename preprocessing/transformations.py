3ae3e7948296ca2efa23059a6153a8b564926e53
Kevin Dunn <kevin.dunn@connectmv.com>
2011-10-27 08:17:44 -0400

    Added transformations

---
+++ preprocessing/transformations.py
@@ -0,0 +1,14 @@
+import numpy as np
+from matplotlib.pylab import plot
+
+x1 = np.arange(1, 6, 0.05)
+x2 = 5 + 2*x1 +8*x1**2
+x1pp = (x1 - np.mean(x1))/np.std(x1)
+x2pp = (x2 - np.mean(x2))/np.std(x2)
+
+x2tr = np.sqrt(x2)
+x2tr_pp = (x2tr - np.mean(x2tr))/np.std(x2tr)
+
+plot(x1pp, x2pp)
+plot(x1pp, x2tr_pp)
+grid
