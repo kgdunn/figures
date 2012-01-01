2e8aab67ad7f374a7a04e888f46e109611cd81a6
Kevin Dunn <kevin.dunn@connectmv.com>
2012-01-01 17:50:52 -0500

    Fixed script for PDF layout

--- visualization/boxplot-for-boards.R
+++ visualization/boxplot-for-boards.R
@@ -7,7 +7,8 @@ first100 <- boards[1:100, 2:7]
 
 # Ignore the first date/time column: using only Pos1, Pos2, ... Pos6 columns
 
-bitmap('boxplot-for-two-by-six-100-boards.png', pointsize=14, res=300, type="png256", width=6, heigh=5)
+bitmap('boxplot-for-two-by-six-100-boards.png', pointsize=14, res=300, 
+       type="png256", width=6, heigh=5)
 par(mar=c(2, 4, 0.2, 0.2))  # (bottom, left, top, right) spacing around plot
 boxplot(first100, ylab="Thickness [mils]")
 dev.off()
