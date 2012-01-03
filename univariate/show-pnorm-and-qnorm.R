be59b6dee7803fb7bca071e796c9f941db71c16b
Kevin Dunn <kevin.dunn@connectmv.com>
2012-01-02 19:43:56 -0500

    Updated LS figure for cumulative distribution

--- univariate/show-pnorm-and-qnorm.R
+++ univariate/show-pnorm-and-qnorm.R
@@ -1,10 +1,12 @@
 
 bitmap('show-pnorm-and-qnorm.png', type="png256", width=10, height=7, res=300, pointsize=14)
+par(mar=c(4, 4.5, 0.2, 0.2))  # (B, L, T, R) par(mar=c(5, 4, 4, 2) + 0.1)
 
 z <- seq(-4, 4, 0.01)
 cumulative.norm = pnorm(z)
 
-plot(z, cumulative.norm, type="l", main="", xlab="z", ylab="Cumulative area under normal distribution",  cex.lab=1.5, cex.main=1.8, lwd=4, cex.sub=1.8, cex.axis=1.8, ylim=c(0, 1))
+plot(z, cumulative.norm, type="l", main="", xlab="z", ylab="Cumulative area under normal distribution",  
+    cex.lab=1.5, cex.main=1.8, lwd=4, cex.sub=1.8, cex.axis=1.8, ylim=c(0, 1))
 
 a1 = -0.5
 arrows(a1, y=-0.2, x1=a1, y1=pnorm(a1), code=0, lwd=2)
@@ -16,5 +18,7 @@ a1 = 1
 arrows(a1, y=0, x1=a1, y1=pnorm(a1), code=1, lwd=2)
 arrows(a1, y=pnorm(a1), x1=-5, y1=pnorm(a1), code=0, lwd=2)
 text(2,0.6, "qnorm(...)", cex=1.5)
+
+abline(v=0, lty=2)
 
 dev.off()
