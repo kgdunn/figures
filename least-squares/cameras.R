a9f50abdb74f4edf1efd583d2c8bfe7cd89ce331
Kevin Dunn <kevin.dunn@connectmv.com>
2012-02-09 07:38:51 -0500

    Added camera data and script

---
+++ least-squares/cameras.R
@@ -0,0 +1,18 @@
+cam <- read.csv('cameras.csv')
+plot(cam$Cameras, cam$Deaths)
+grid()
+#identify(cam$Cameras, cam$Deaths, cam$Country)
+
+model.all <- lm(cam$Deaths ~ cam$Cameras)
+summary(model.all)
+confint(model.all)
+
+# Non-parametric model
+plot(cam$Cameras, cam$Deaths)
+grid()
+lines(lowess(cam$Cameras, cam$Deaths))
+
+library(car)
+qqPlot(resid(model.all), labels=cam$Country, id.method="identify")
+
+influenceIndexPlot(model.all)
