f4c942c2c63a27251384ce1cb386e799a2122c3f
Kevin Dunn <kevin.dunn@connectmv.com>
2012-04-15 19:08:39 -0400

    Added blender efficiency code

---
+++ least-squares/blender-efficiency.R
@@ -0,0 +1,17 @@
+data <- read.csv('http://datasets.connectmv.com/file/blender-efficiency.csv')
+summary(data)
+cov(data)
+model<-lm(data$BlendingEfficiency ~ data$ParticleSize + data$MixerDiameter + 
+                                     data$MixerRotation + data$BlendingTime)
+summary(model)
+
+confint(model, level=0.90)
+
+library(car)
+qqPlot(model, id.n=1)
+
+# Omit point 8:
+model.update <- lm(model, subset=-c(8))
+summary(model.update)
+qqPlot(model.update)
+confint(model.update, level=0.90)
