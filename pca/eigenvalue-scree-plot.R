c349374f543874d8983dd3ce4103d8e979fa525d
Kevin Dunn <kevin.dunn@connectmv.com>
2012-05-13 21:45:17 -0400

    Added/update scree plot

--- pca/eigenvalue-scree-plot.R
+++ pca/eigenvalue-scree-plot.R
@@ -15,5 +15,14 @@ barchart(as.matrix(ev$values[1:K] / ev$sum * 100), horizontal=FALSE,
         col=0, ylab=list(label="Proportion of variance explained (%)", cex=1.5), 
         xlab=list(label="Component number", cex=1.5), 
         scales=list(x=list(labels=seq(1,K), cex=1.5), y=list(cex=1.5)),
-        main=list("Pareto plot (scree plot) of R2 per component", cex=1.5))
+        main=list(expression("Pareto plot (scree plot) of R"^2*" per component"), cex=1.5))
 dev.off()
+
+bitmap('eigenvalue-scree-plot-cumulative.png', type="png256", width=10, height=5, res=300, pointsize=14)
+barchart(as.matrix(cumsum(ev$values[1:K]) / ev$sum * 100), horizontal=FALSE, 
+        col=0, ylab=list(label="Cumulative variance explained (%)", cex=1.5), 
+        xlab=list(label="Component number", cex=1.5), 
+        scales=list(x=list(labels=seq(1,K), cex=1.5), y=list(cex=1.5)),
+        main=list(expression("Pareto plot (scree plot) of R"^2*" per component"), cex=1.5))
+dev.off()
+
