f2af6dfdde8aac56c76cd2824d153d01a3f5d9d9
Kevin Dunn <kgdunn@gmail.com>
2013-01-25 20:52:52 -0500

    Extra visualization figures

---
+++ visualization/board-thickness-boxplot-assignment-2013.R
@@ -0,0 +1,14 @@
+boards <- read.csv('http://datasets.connectmv.com/file/six-point-board-thickness.csv')
+summary(boards)
+
+# Ignore the first date/time column: using only Pos1, Pos2, ... Pos6 columns
+first100 <- boards[1:100, 2:7]
+later100 <- boards[4800:4900, 2:7]
+
+bitmap('boxplot-for-two-by-six-boards-assign1-2013.png', pointsize=14, res=300,
+       type="png256", width=10, height=5)
+layout(matrix(c(1,2), 1, 2)) # layout plot in a 1x2 matrix
+par(mar=c(2, 4, 0.2, 0.2))   # (bottom, left, top, right) spacing around plot
+boxplot(first100, ylab="Thickness [mils]", ylim=c(1400, 1850))
+boxplot(later100, ylab="Thickness [mils]", ylim=c(1400, 1850))
+dev.off()
