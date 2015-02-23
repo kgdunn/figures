73e4b23df89258f42eb70d52f51ed6d9e0185700
Kevin Dunn <kgdunn@gmail.com>
2015-02-23 12:53:02 -0500

    Slides for week 7A

---
+++ least-squares/time-series-autocorrelated-example.R
@@ -0,0 +1,14 @@
+e<-read.csv('http://datasets.connectmv.com/file/gas-furnace.csv')
+
+bitmap('time-series-cycle.png', type="png256", width=14, height=4, res=300, pointsize=14)
+par(mar=c(4.2, 4.2, 1.5, 0.5))  # (bottom, left, top, right); defaults are par(mar=c(5, 4, 4, 2) + 0.1)
+#layout(matrix(c(1,2), 1, 2))
+plot(e$InputGasRate+10, type="l", cex.lab=1.5, cex.main=1.2, cex.sub=1.8, cex.axis=1.8, 
+     main="Gas flow rate over time",
+     xlab="Time",
+     ylab="Gas flow rate")
+
+dev.off()
+
+
+
