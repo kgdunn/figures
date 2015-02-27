5152ac261bb112c1420ec8ace51168a0c72d36aa
Kevin Dunn <kgdunn@gmail.com>
2015-02-27 07:17:42 -0500

    Slides for week 7B

--- least-squares/show-MLR-confint.R
+++ least-squares/show-MLR-confint.R
@@ -1,8 +1,33 @@
 set.seed(37)
 N=10
+
+
 volume <- round(rnorm(N, 10, 3))
 temperature <- round(rnorm(N, 273+32,5))
 y <- round(-0.5*volume + 4.2*temperature + rnorm(N,125) )
+
+# ... code to load the data ...
+
 model <- lm(y ~ volume + temperature)
 summary(model)
 confint(model)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
